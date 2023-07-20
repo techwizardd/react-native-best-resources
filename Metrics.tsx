60c5451df7469802b562386da667bcea1e66a368
purevsuren <35187128+purevsurena@users.noreply.github.com>
2023-07-21 01:43:48 +0800

    Add Responsive Scaling Functions for Font Size and Border Radius in React Native
    This pull request expands the capabilities of the existing scale function by introducing two new responsive scaling functions, mScale and borderRadiusScale, specifically designed for font size and border radius adjustments in React Native applications.

--- Metrics.tsx
+++ Metrics.tsx
@@ -7,4 +7,8 @@ const guidelineBaseWidth = 375;
 // width, height, margin, padding, likewise
 const scale = (size: number) => (width / guidelineBaseWidth) * size;
 
-export { scale };
+// for fon-size, border-radius, likewise
+const mScale = (size: number, factor = 0.5) =>
+  size + (scale(size) - size) * factor;
+
+export { scale, mScale };
